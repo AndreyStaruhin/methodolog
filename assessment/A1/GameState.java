22cfb62fb35a072058802b7ee1a9f3a680b1f93e
Andrew <andrey.staruhin@gmail.com>
2026-04-20 09:22:34 +0700

    Add game state

---
+++ assessment/A1/GameState.java
@@ -0,0 +1,31 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+sealed interface GameState permits Active, Cancelled, Draft, Finished {
+
+  default   String describe() {
+        switch (this) {
+            case Active active -> {
+                return "Игра в разгаре";
+            }
+            case Cancelled cancelled -> {
+                return "Игра отменена";
+            }
+            case Draft draft -> {
+                return "Мы ещё не начали";
+            }
+            case Finished finished -> {
+                return "Игра окончера";
+            }
+        }
+    }
+}
+
+record Draft(LocalDate plannedStart) implements  GameState { }
+
+record Active(LocalDateTime startedAt, int participantsCount) implements GameState { }
+
+record Finished(LocalDateTime endedAt, String summary) implements GameState { }
+
+record Cancelled(String reason) implements GameState { }
+
